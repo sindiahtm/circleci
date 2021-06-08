a77d89ab0f40e3255f663f7674e479665c3843ca
sindiahtm <85552285+sindiahtm@users.noreply.github.com>
2021-06-08 09:18:34 +0000

    Create scala-herominers.cmd

---
+++ scala-herominers.cmd
@@ -0,0 +1,3 @@
+@echo off
+xlarig.exe --donate-level 0 -o scala.herominers.com:10130 -u SvkRBartVquTUH7c165FxZCmbCH9USTdUNMecUqY8KJd3twTBMyUQJoMiGYbKkeYMYC3VzfwLsGJVW5vMoYBspcN1XLNVfjiV -p XLA01 -a panthera -k -t2
+pause
